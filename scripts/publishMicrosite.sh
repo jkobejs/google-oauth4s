4d034d05daa3f0f581a117b9be399cd90eff340d
Josip Grgurica <josip.grgurica@gmail.com>
2019-07-09 17:57:55 +0200

    after_success

--- scripts/publishMicrosite.sh
+++ scripts/publishMicrosite.sh
@@ -2,7 +2,7 @@
 set -e
 
 git config --global user.email "josip.grgurica@gmail.com"
-git config --global user.name "Josip Grgurica"
+git config --global user.name "jkobejs"
 git config --global push.default simple
 
 sbt publishMicrosite
