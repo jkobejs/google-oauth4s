0b70a34a97d427b199e649c7cce2249121cf84df
Josip Grgurica <josip.grgurica@gmail.com>
2019-07-10 12:58:00 +0200

    remove site publishing from CI

--- scripts/decrypt-keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-openssl aes-256-cbc -K $encrypted_c92f00363573_key -iv $encrypted_c92f00363573_iv -in travis-deploy-key.enc -out travis-deploy-key -d;
-chmod 600 travis-deploy-key;
-cp travis-deploy-key ~/.ssh/id_rsa;
